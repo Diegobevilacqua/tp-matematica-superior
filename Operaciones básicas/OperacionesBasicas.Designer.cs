88e300eb246770286a37d797518d7a7c3a70cf12
Diegobevilacqua <dfb_98@hotmail.com>
2019-04-06 01:01:25 -0300

    Carpetas para cada tipo de operación y diseño del form de suma.

---
+++ Operaciones básicas/OperacionesBasicas.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NCOM.Operaciones_básicas
+{
+    partial class OperacionesBasicas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxOperacion = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.buttonAtras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(142, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Operaciones básicas";
+            // 
+            // comboBoxOperacion
+            // 
+            this.comboBoxOperacion.FormattingEnabled = true;
+            this.comboBoxOperacion.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicación",
+            "División"});
+            this.comboBoxOperacion.Location = new System.Drawing.Point(316, 102);
+            this.comboBoxOperacion.Name = "comboBoxOperacion";
+            this.comboBoxOperacion.Size = new System.Drawing.Size(280, 21);
+            this.comboBoxOperacion.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(24, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seleccione la operación a realizar:";
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAceptar.Location = new System.Drawing.Point(493, 165);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(103, 27);
+            this.buttonAceptar.TabIndex = 5;
+            this.buttonAceptar.Text = "Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            this.buttonAceptar.Click += new System.EventHandler(this.ButtonAceptar_Click);
+            // 
+            // buttonAtras
+            // 
+            this.buttonAtras.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAtras.Location = new System.Drawing.Point(12, 165);
+            this.buttonAtras.Name = "buttonAtras";
+            this.buttonAtras.Size = new System.Drawing.Size(103, 27);
+            this.buttonAtras.TabIndex = 6;
+            this.buttonAtras.Text = "Atrás";
+            this.buttonAtras.UseVisualStyleBackColor = true;
+            this.buttonAtras.Click += new System.EventHandler(this.ButtonAtras_Click);
+            // 
+            // OperacionesBasicas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(610, 210);
+            this.Controls.Add(this.buttonAtras);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.comboBoxOperacion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "OperacionesBasicas";
+            this.Text = "Operaciones básicas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxOperacion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.Button buttonAtras;
+    }
+}
---
+++ Operaciones básicas/Suma.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace NCOM.Operaciones_básicas
+{
+    partial class Suma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.buttonAtras = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonBinomica1 = new System.Windows.Forms.Button();
+            this.buttonPolar1 = new System.Windows.Forms.Button();
+            this.buttonPolar2 = new System.Windows.Forms.Button();
+            this.buttonBinomica2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(64, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Suma de números complejos";
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAceptar.Location = new System.Drawing.Point(474, 347);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(103, 27);
+            this.buttonAceptar.TabIndex = 4;
+            this.buttonAceptar.Text = "Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            // 
+            // buttonAtras
+            // 
+            this.buttonAtras.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAtras.Location = new System.Drawing.Point(18, 347);
+            this.buttonAtras.Name = "buttonAtras";
+            this.buttonAtras.Size = new System.Drawing.Size(103, 27);
+            this.buttonAtras.TabIndex = 5;
+            this.buttonAtras.Text = "Atrás";
+            this.buttonAtras.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(14, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "1er número complejo:";
+            // 
+            // buttonBinomica1
+            // 
+            this.buttonBinomica1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBinomica1.Location = new System.Drawing.Point(253, 103);
+            this.buttonBinomica1.Name = "buttonBinomica1";
+            this.buttonBinomica1.Size = new System.Drawing.Size(149, 27);
+            this.buttonBinomica1.TabIndex = 7;
+            this.buttonBinomica1.Text = "Forma binómica";
+            this.buttonBinomica1.UseVisualStyleBackColor = true;
+            // 
+            // buttonPolar1
+            // 
+            this.buttonPolar1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPolar1.Location = new System.Drawing.Point(428, 103);
+            this.buttonPolar1.Name = "buttonPolar1";
+            this.buttonPolar1.Size = new System.Drawing.Size(149, 27);
+            this.buttonPolar1.TabIndex = 8;
+            this.buttonPolar1.Text = "Forma polar";
+            this.buttonPolar1.UseVisualStyleBackColor = true;
+            // 
+            // buttonPolar2
+            // 
+            this.buttonPolar2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPolar2.Location = new System.Drawing.Point(428, 210);
+            this.buttonPolar2.Name = "buttonPolar2";
+            this.buttonPolar2.Size = new System.Drawing.Size(149, 27);
+            this.buttonPolar2.TabIndex = 11;
+            this.buttonPolar2.Text = "Forma polar";
+            this.buttonPolar2.UseVisualStyleBackColor = true;
+            // 
+            // buttonBinomica2
+            // 
+            this.buttonBinomica2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBinomica2.Location = new System.Drawing.Point(253, 210);
+            this.buttonBinomica2.Name = "buttonBinomica2";
+            this.buttonBinomica2.Size = new System.Drawing.Size(149, 27);
+            this.buttonBinomica2.TabIndex = 10;
+            this.buttonBinomica2.Text = "Forma binómica";
+            this.buttonBinomica2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(14, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "2do número complejo:";
+            // 
+            // Suma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(597, 393);
+            this.Controls.Add(this.buttonPolar2);
+            this.Controls.Add(this.buttonBinomica2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonPolar1);
+            this.Controls.Add(this.buttonBinomica1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonAtras);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.label1);
+            this.Name = "Suma";
+            this.Text = "Suma";
+            this.Load += new System.EventHandler(this.Suma_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.Button buttonAtras;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonBinomica1;
+        private System.Windows.Forms.Button buttonPolar1;
+        private System.Windows.Forms.Button buttonPolar2;
+        private System.Windows.Forms.Button buttonBinomica2;
+        private System.Windows.Forms.Label label3;
+    }
+}
