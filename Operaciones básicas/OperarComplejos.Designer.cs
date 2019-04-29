8deba7b3f64c2739cfce05404031659232929e2d
Diegobevilacqua <dfb_98@hotmail.com>
2019-04-28 23:26:15 -0300

    ...

--- Operaciones básicas/OperarComplejos.Designer.cs
+++ Operaciones básicas/OperarComplejos.Designer.cs
@@ -74,11 +74,11 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Light", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(55, 23);
+            this.label3.Location = new System.Drawing.Point(118, 24);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(433, 46);
+            this.label3.Size = new System.Drawing.Size(309, 46);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Operaciones entre complejos";
+            this.label3.Text = "Operaciones básicas";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // pictureAtras
