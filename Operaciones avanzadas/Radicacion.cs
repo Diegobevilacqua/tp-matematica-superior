5b9f2daa4c5531001725510316f3f9767b972af7
Diegobevilacqua <dfb_98@hotmail.com>
2019-05-12 19:07:10 -0300

    Agrego radicación

---
+++ Operaciones avanzadas/Radicacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCOM.Operaciones_avanzadas
+{
+    public static class Radicacion
+    {
+        public static ComplejoPolar[] RaizNatural(ComplejoPolar complejoPolar, double orden)
+        {
+            ComplejoPolar[] raices = new ComplejoPolar[(int)orden];
+
+            for (int k = 0; k < orden - 1; k++)
+            {
+                double modulo = Math.Pow(complejoPolar.Modulo, 1 / orden);
+                double argumento = (complejoPolar.Argumento + 2 * k * Math.PI) / orden;
+                raices[k] = new ComplejoPolar(modulo, argumento);
+            }
+
+            return raices;
+        }
+    }
+}
