eecb9f86bb7516da4db4b50ee3cffa7f59563376
Diegobevilacqua <dfb_98@hotmail.com>
2019-05-13 00:47:15 -0300

    Cambio botones de potenciación y radicación por botón de "Operaciones avanzadas"

--- Inicio.Designer.cs
+++ Inicio.Designer.cs
@@ -32,15 +32,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureOpBasicas = new System.Windows.Forms.PictureBox();
             this.pictureSFasores = new System.Windows.Forms.PictureBox();
-            this.pictureRadic = new System.Windows.Forms.PictureBox();
-            this.labelRad = new System.Windows.Forms.Label();
             this.labelPot = new System.Windows.Forms.Label();
             this.picturePot = new System.Windows.Forms.PictureBox();
             this.labelSFasores = new System.Windows.Forms.Label();
             this.labelOpBasicas = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureOpBasicas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureSFasores)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureRadic)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picturePot)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +46,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Light", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(200, 28);
+            this.label1.Location = new System.Drawing.Point(12, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(328, 46);
             this.label1.TabIndex = 0;
@@ -59,8 +56,8 @@
             // 
             this.pictureOpBasicas.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureOpBasicas.Image = ((System.Drawing.Image)(resources.GetObject("pictureOpBasicas.Image")));
-            this.pictureOpBasicas.Location = new System.Drawing.Point(58, 136);
-            this.pictureOpBasicas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureOpBasicas.Location = new System.Drawing.Point(37, 120);
+            this.pictureOpBasicas.Margin = new System.Windows.Forms.Padding(2);
             this.pictureOpBasicas.Name = "pictureOpBasicas";
             this.pictureOpBasicas.Size = new System.Drawing.Size(276, 54);
             this.pictureOpBasicas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -72,42 +69,14 @@
             // 
             this.pictureSFasores.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureSFasores.Image = ((System.Drawing.Image)(resources.GetObject("pictureSFasores.Image")));
-            this.pictureSFasores.Location = new System.Drawing.Point(373, 136);
-            this.pictureSFasores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureSFasores.Location = new System.Drawing.Point(38, 297);
+            this.pictureSFasores.Margin = new System.Windows.Forms.Padding(2);
             this.pictureSFasores.Name = "pictureSFasores";
             this.pictureSFasores.Size = new System.Drawing.Size(276, 54);
             this.pictureSFasores.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureSFasores.TabIndex = 2;
             this.pictureSFasores.TabStop = false;
             this.pictureSFasores.Click += new System.EventHandler(this.PictureSFasores_Click);
-            // 
-            // pictureRadic
-            // 
-            this.pictureRadic.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureRadic.Image = ((System.Drawing.Image)(resources.GetObject("pictureRadic.Image")));
-            this.pictureRadic.Location = new System.Drawing.Point(373, 241);
-            this.pictureRadic.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureRadic.Name = "pictureRadic";
-            this.pictureRadic.Size = new System.Drawing.Size(276, 54);
-            this.pictureRadic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureRadic.TabIndex = 3;
-            this.pictureRadic.TabStop = false;
-            this.pictureRadic.Click += new System.EventHandler(this.PictureRadic_Click);
-            // 
-            // labelRad
-            // 
-            this.labelRad.AutoSize = true;
-            this.labelRad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(21)))), ((int)(((byte)(80)))));
-            this.labelRad.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.labelRad.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelRad.ForeColor = System.Drawing.Color.White;
-            this.labelRad.Location = new System.Drawing.Point(450, 253);
-            this.labelRad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.labelRad.Name = "labelRad";
-            this.labelRad.Size = new System.Drawing.Size(120, 30);
-            this.labelRad.TabIndex = 4;
-            this.labelRad.Text = "Radicación";
-            this.labelRad.Click += new System.EventHandler(this.LaberRad_Click);
             // 
             // labelPot
             // 
@@ -116,22 +85,22 @@
             this.labelPot.Cursor = System.Windows.Forms.Cursors.Hand;
             this.labelPot.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelPot.ForeColor = System.Drawing.Color.White;
-            this.labelPot.Location = new System.Drawing.Point(130, 253);
+            this.labelPot.Location = new System.Drawing.Point(58, 221);
             this.labelPot.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelPot.Name = "labelPot";
-            this.labelPot.Size = new System.Drawing.Size(140, 30);
+            this.labelPot.Size = new System.Drawing.Size(243, 30);
             this.labelPot.TabIndex = 6;
-            this.labelPot.Text = "Potenciación";
+            this.labelPot.Text = "Operaciones avanzadas";
             this.labelPot.Click += new System.EventHandler(this.LabelPot_Click);
             // 
             // picturePot
             // 
             this.picturePot.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picturePot.Image = ((System.Drawing.Image)(resources.GetObject("picturePot.Image")));
-            this.picturePot.Location = new System.Drawing.Point(58, 241);
-            this.picturePot.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picturePot.Location = new System.Drawing.Point(37, 209);
+            this.picturePot.Margin = new System.Windows.Forms.Padding(2);
             this.picturePot.Name = "picturePot";
-            this.picturePot.Size = new System.Drawing.Size(276, 54);
+            this.picturePot.Size = new System.Drawing.Size(276, 56);
             this.picturePot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picturePot.TabIndex = 5;
             this.picturePot.TabStop = false;
@@ -144,7 +113,7 @@
             this.labelSFasores.Cursor = System.Windows.Forms.Cursors.Hand;
             this.labelSFasores.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold);
             this.labelSFasores.ForeColor = System.Drawing.Color.White;
-            this.labelSFasores.Location = new System.Drawing.Point(437, 145);
+            this.labelSFasores.Location = new System.Drawing.Point(97, 308);
             this.labelSFasores.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelSFasores.Name = "labelSFasores";
             this.labelSFasores.Size = new System.Drawing.Size(163, 32);
@@ -159,7 +128,7 @@
             this.labelOpBasicas.Cursor = System.Windows.Forms.Cursors.Hand;
             this.labelOpBasicas.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelOpBasicas.ForeColor = System.Drawing.Color.White;
-            this.labelOpBasicas.Location = new System.Drawing.Point(87, 145);
+            this.labelOpBasicas.Location = new System.Drawing.Point(66, 129);
             this.labelOpBasicas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelOpBasicas.Name = "labelOpBasicas";
             this.labelOpBasicas.Size = new System.Drawing.Size(235, 32);
@@ -172,13 +141,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(46)))), ((int)(((byte)(59)))));
-            this.ClientSize = new System.Drawing.Size(697, 340);
+            this.ClientSize = new System.Drawing.Size(353, 384);
             this.Controls.Add(this.labelOpBasicas);
             this.Controls.Add(this.labelSFasores);
             this.Controls.Add(this.labelPot);
             this.Controls.Add(this.picturePot);
-            this.Controls.Add(this.labelRad);
-            this.Controls.Add(this.pictureRadic);
             this.Controls.Add(this.pictureSFasores);
             this.Controls.Add(this.pictureOpBasicas);
             this.Controls.Add(this.label1);
@@ -186,7 +153,6 @@
             this.Text = "Inicio";
             ((System.ComponentModel.ISupportInitialize)(this.pictureOpBasicas)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureSFasores)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureRadic)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picturePot)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -198,8 +164,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureOpBasicas;
         private System.Windows.Forms.PictureBox pictureSFasores;
-        private System.Windows.Forms.PictureBox pictureRadic;
-        private System.Windows.Forms.Label labelRad;
         private System.Windows.Forms.Label labelPot;
         private System.Windows.Forms.PictureBox picturePot;
         private System.Windows.Forms.Label labelSFasores;
--- Operaciones avanzadas/RaicesPrimitivas.cs
+++ Operaciones avanzadas/RaicesPrimitivas.cs
@@ -8,15 +8,16 @@ namespace NCOM.Operaciones_avanzadas
 {
     public static class RaicesPrimitivas
     {
-        
         public static ComplejoPolar[] Calcular(int orden)
         {
             ComplejoPolar[] raices = new ComplejoPolar[orden];
             
             for (int k = 0; k < orden - 1; k++)
             {
-
+                raices[k] = new ComplejoPolar(1, ((2 * Math.PI) * k) / orden);
             }
+
+            return raices;
         }
     }
 }
